367c2e890c9facf18326e04d13d3df1e068640d2
심승용 <seungyong.shim@mirero.co.kr>
2020-09-24 10:16:16 +0900

    프로젝트 추가

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Rx_Console_PressKey_POC
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## 목표
 - Rx를 사용하여 KeyPress를 처리한다.
+
+## 참조
+- https://stackoverflow.com/questions/10675451/iobservable-of-keys-pressed
