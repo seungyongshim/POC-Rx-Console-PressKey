5ec30f20f487c673c48fd080fc573d1f768650e6
심승용 <seungyong.shim@mirero.co.kr>
2020-09-24 10:31:46 +0900

    코드 작성

--- Program.cs
+++ Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
 
 namespace Rx_Console_PressKey_POC
 {
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var keys = KeyPresses().ToObservable();
+
+            keys.Throttle(TimeSpan.FromMilliseconds(100))
+                .Subscribe(key => Console.WriteLine("Pressed: {0}", key.Key));
+
+
+        }
+
+        static IEnumerable<ConsoleKeyInfo> KeyPresses()
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey();
+                yield return key;
+            } while (key.Key != ConsoleKey.Escape);
         }
     }
 }
--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 
 ## 참조
 - https://stackoverflow.com/questions/10675451/iobservable-of-keys-pressed
+
+## Debounce
+- http://reactivex.io/documentation/operators/debounce.html
+- 특정 시간 범위가 지난 경우에만 Observable에서 항목을 내 보낸다.
+
+![](./images/1.png)
